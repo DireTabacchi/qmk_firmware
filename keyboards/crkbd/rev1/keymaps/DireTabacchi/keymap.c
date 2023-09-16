cbf6ed85eea9c33c02f9615457626d0377ef6d8c
Nathaniel Tabacchi <ntabacchi@outlook.com>
2023-09-15 20:16:30 -0400

    Rearranged keymap for Dvorak. TODO: Create "shortcuts" layer.

--- keyboards/crkbd/rev1/keymaps/DireTabacchi/keymap.c
+++ keyboards/crkbd/rev1/keymaps/DireTabacchi/keymap.c
@@ -51,13 +51,13 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BL] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, TG(_NL),
+       KC_TAB, KC_QUOT, KC_COMM,  KC_DOT,    KC_P,    KC_Y,                         KC_F,    KC_G,    KC_C,    KC_R,    KC_L, KC_SLSH,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_CAPS,    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                         KC_M,    KC_N,    KC_E,    KC_I,    KC_O, KC_QUOT,
+      KC_CAPS,    KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                         KC_D,    KC_H,    KC_T,    KC_N,    KC_S, KC_MINS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
+      KC_LSFT, KC_SCLN,    KC_Q,    KC_J,    KC_K,    KC_X,                         KC_B,    KC_M,    KC_W,    KC_V,    KC_Z, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                         KC_LGUI,TD(TD_THSL),KC_SPC,    KC_BSPC, KC_LCTL, KC_RALT
+                                         KC_LGUI,TD(TD_THSL),KC_SPC,    KC_BSPC, KC_LCTL, TG(_NL)
                                       //`--------------------------'  `--------------------------'
 
   ),
@@ -68,21 +68,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL, KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN,  KC_EQL,                      KC_AMPR,    KC_4,    KC_5,    KC_6, KC_PLUS, KC_ASTR,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,  KC_GRV, KC_TILD, KC_LCBR, KC_RCBR, KC_PIPE,                         KC_0,    KC_1,    KC_2,    KC_3, KC_BSLS, KC_RSFT,
+      KC_LSFT,  KC_GRV, KC_TILD, KC_LCBR, KC_RCBR, KC_PIPE,                         KC_0,    KC_1,    KC_2,    KC_3, KC_BSLS, KC_SLSH,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, KC_TRNS,  KC_SPC,     KC_ENT, KC_LGUI, KC_RALT
+                                          KC_LSFT, KC_TRNS,  KC_SPC,     KC_ENT, KC_LGUI, KC_RALT
                                       //`--------------------------'  `--------------------------'
   ),
 
     [_NL] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_ACL0, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TG(_NL),
+       KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_ACL0, XXXXXXX, KC_WH_D, KC_WH_U, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, KC_PSCR,                      KC_ACL1, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_ACL2, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX,
+      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_ACL2, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_RALT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, KC_TRNS,  KC_SPC,    KC_BTN1, KC_BTN2, KC_RALT
+                                          KC_LGUI, KC_TRNS,  KC_SPC,    KC_BTN1, KC_BTN2, TG(_NL)
                                       //`--------------------------'  `--------------------------'
   )
 };
